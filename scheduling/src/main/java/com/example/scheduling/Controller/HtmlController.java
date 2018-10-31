c611b9d9fefffb548ac03c8fc2e2adda7ba0b088
R9python <483983@qq.com>
2018-11-01 01:00:29 +0800

    demo

--- scheduling/src/main/java/com/example/scheduling/Controller/HtmlController.java
+++ scheduling/src/main/java/com/example/scheduling/Controller/HtmlController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class TestController {
+public class HtmlController {
 
     /**
      * 本地访问内容地址 ：http://localhost:8080/scheduling
--- scheduling/src/main/java/com/example/scheduling/schedule/controller/ScheduleController.java
+++ scheduling/src/main/java/com/example/scheduling/schedule/controller/ScheduleController.java
@@ -1,12 +1,15 @@
 package com.example.scheduling.schedule.controller;
 
 
+import java.time.LocalDate;
+
 import com.example.scheduling.schedule.entity.Schedule;
 import com.example.scheduling.schedule.mapper.ScheduleMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,10 +26,16 @@ public class ScheduleController {
     @Autowired 
     ScheduleMapper mapper;
 
-    @RequestMapping("/schedule/save")
-    public void ScheduleSave(){
-        Schedule entity = new Schedule();
+    @RequestMapping(value="/schedule/save/{userid}", method=RequestMethod.GET)
+    public void ScheduleSave(@PathVariable long userid){
+        Schedule schedule = new Schedule();
+        LocalDate today = LocalDate.now();
+        schedule.setUserId(userid);
+        schedule.setIsRest(1);
+        schedule.setWorkDate(today);
         // entity.setUserId(1);
-        mapper.insert(entity);
+        mapper.insert(schedule);
     }
+
+
 }
